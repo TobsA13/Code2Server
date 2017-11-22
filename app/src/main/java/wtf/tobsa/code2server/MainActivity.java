4f4733892459955ced6bf882f1568c82d44895ab
Tobias Albert <github@tobsa.wtf>
2017-11-23 00:11:38 +0100

    Initial commit

---
+++ app/src/main/java/wtf/tobsa/code2server/MainActivity.java
@@ -0,0 +1,68 @@
+package wtf.tobsa.code2server;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    RequestQueue queue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new IntentIntegrator(this).initiateScan();
+        queue = Volley.newRequestQueue(this);
+
+    }
+
+
+
+    // Get the results:
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                sendResult(result);
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    protected void sendResult(IntentResult result){
+        String url ="http://tobsa.wtf/logBarcode.php?pin=324&code=" + result.getContents() + "&format=" + result.getFormatName();
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(MainActivity.this, "Upload ok", Toast.LENGTH_LONG).show();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this, "Upload fehlgeschlagen", Toast.LENGTH_LONG).show();
+            }
+        });
+        queue.add(stringRequest);
+
+    }
+}
